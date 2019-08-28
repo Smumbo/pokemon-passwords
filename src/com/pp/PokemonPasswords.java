296bdc7d649ed46536c833eb249c0fbb804a1051
Jonah <jonahvenglarcik@gmail.com>
2019-08-28 16:50:03 -0700

    Added header and file opener method

---
+++ src/com/pp/PokemonPasswords.java
@@ -0,0 +1,56 @@
+package com.pp;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/*
+ * AUTHOR: Jonah Venglarcik
+ * FILE: PokemonPasswords.java
+ * PURPOSE: Password generator that uses adjectives, symbols, Pokémon names, and numbers to make
+ * memorable passwords not solely based on dictionary terms. Uses a few configurable input files.
+ *
+ * USAGE:
+ * inputWords inputSymbols inputPokemonNames
+ *
+ * Where the input files are .txt files with one term per line, like so:
+ * ------------------ infile ------------------
+ * ...
+ * Chansey
+ * Tangela
+ * Kangaskhan
+ * Horsea
+ * Seadra
+ * Goldeen
+ * Seaking
+ * Staryu
+ * Starmie
+ * Mr. Mime
+ * ...
+ * --------------------------------------------
+ */
+public class PokemonPasswords {
+  public static void main(String[] args) {
+    // test comment
+  }
+
+  /**
+   * Purpose: Creates input file Scanner using input fileName
+   *
+   * @param fileName
+   *            is the name of the file to open.
+   *
+   * @return file
+   *         is the Scanner containing the input file.
+   *
+   */
+  public static Scanner openFile(String fileName) {
+    Scanner file = null;
+    try {
+      file = new Scanner(new File(fileName));
+    } catch (FileNotFoundException error) {
+      error.printStackTrace();
+    }
+    return file;
+  }
+}
--- src/com/pp/pokemonPasswords.java
+++
@@ -1,8 +0,0 @@
-package com.pp;
-
-public class pokemonPasswords {
-    public static void main(String[] args) {
-        // write your code here
-        // test comment
-    }
-}
